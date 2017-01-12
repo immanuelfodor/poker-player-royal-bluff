4b472fc980a54972734bece07430663a8ac67d0f
aniko.fenyvesi <aniko.fenyvesi@codecool.com>
2017-01-12 14:11:36 +0100

    adding test for not the same sets

--- test_player.py
+++ test_player.py
@@ -30,5 +30,54 @@ class TestPlayer(unittest.TestCase):
         player = Player()
         self.assertTrue(player.same(cards1, cards2))
 
+   def test_same_diff(self):
+        cards1 = [
+            {
+                "rank": "10",
+                "suit": "hearts"
+            },
+            {
+                "rank": "J",
+                "suit": "hearts"
+            },
+            {
+                "rank": "Q",
+                "suit": "hearts"
+            },
+            {
+                "rank": "K",
+                "suit": "hearts"
+            },
+            {
+                "rank": "A",
+                "suit": "hearts"
+            }
+            ]
+        cards2 = [
+            {
+                "rank": "10",
+                "suit": "clubs"
+            },
+            {
+                "rank": "J",
+                "suit": "hearts"
+            },
+            {
+                "rank": "Q",
+                "suit": "hearts"
+            },
+            {
+                "rank": "K",
+                "suit": "hearts"
+            },
+            {
+                "rank": "A",
+                "suit": "hearts"
+            }
+            ]
+
+        player = Player()
+        self.assertFalse(player.same(cards1, cards2))
+
 if __name__ == '__main__':
     unittest.main()
